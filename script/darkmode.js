94276672302d222cb6c70d232479208bcf5530cf
Facronactz <facronactz@gmail.com>
2021-11-24 16:03:46 +0700

    fix dark mode

--- script/darkmode.js
+++ script/darkmode.js
@@ -20,3 +20,20 @@ function darkMode() {
   ["bg-light", "navbar-light"].map((v) => nav.toggle(v));
   ["bg-dark", "navbar-dark"].map((v) => nav.toggle(v));
 }
+
+function isdarkMode(){
+  if (localStorage.dark) {
+    var element = document.body;
+    var c = document.getElementsByClassName("card")
+
+    for (let index = 0; index < c.length; index++) {
+          ["bg-dark", "text-white", "border-light", "border-dark"].map((v) => c[index].classList.toggle(v));
+        }
+
+        var nav = document.getElementById("nav").classList;
+        //   element.classList.toggle("bg-dark text-white");
+        ["bg-dark", "text-white"].map((v) => element.classList.toggle(v));
+        ["bg-light", "navbar-light"].map((v) => nav.toggle(v));
+        ["bg-dark", "navbar-dark"].map((v) => nav.toggle(v));
+  }
+}
