b4d91a0068686809193af04da1e23335d022b067
Facronactz <facronactz@gmail.com>
2021-11-24 15:19:17 +0700

    add darkmode

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,11 +1,13 @@
 function darkMode() {
   var element = document.body;
-  var card = document.getElementById("card").classList;
+  var c = document.getElementsByClassName("card")
+  for (let index = 0; index < c.length; index++) {
+    ["bg-dark", "text-white", "border-light", "border-dark"].map((v) => c[index].classList.toggle(v));
+  }
+  
   var nav = document.getElementById("nav").classList;
   //   element.classList.toggle("bg-dark text-white");
   ["bg-dark", "text-white"].map((v) => element.classList.toggle(v));
   ["bg-light", "navbar-light"].map((v) => nav.toggle(v));
   ["bg-dark", "navbar-dark"].map((v) => nav.toggle(v));
-  card.toggle("border-dark");
-  ["bg-dark", "text-white", "border-light"].map((v) => card.toggle(v));
 }
