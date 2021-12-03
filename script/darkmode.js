cbfe0633ff991aacf1ee13ac01357e3ed7aeb898
Facronactz <facronactz@gmail.com>
2021-12-04 02:36:37 +0700

    refactor and noto

--- script/darkmode.js
+++ script/darkmode.js
@@ -4,12 +4,18 @@ function darkMode() {
   } else {
     localStorage.dark = true;
   }
+  toogleDark();
+}
 
+function isdarkMode() {
+  if (localStorage.dark) {
+    toogleDark();
+  }
+}
+
+function toogleDark() {
   var element = document.body;
   var c = document.getElementsByClassName("card");
-  // c.push(document.getElementsByClassName("card-header"));
-  // c.push(document.getElementsByClassName("card-body"));
-  // c.push(document.getElementsByClassName("card-footer"));
 
   for (let index = 0; index < c.length; index++) {
     ["bg-dark", "text-white", "border-light", "border-dark"].map((v) =>
@@ -22,7 +28,7 @@ function darkMode() {
   ["bg-dark", "text-white"].map((v) => element.classList.toggle(v));
   ["bg-light", "navbar-light"].map((v) => nav.toggle(v));
   ["bg-dark", "navbar-dark"].map((v) => nav.toggle(v));
-  
+
   var i = document.getElementById("i").classList;
   i.toggle("i-black");
 
@@ -30,31 +36,10 @@ function darkMode() {
   ["bg-light", "navbar-light"].map((v) => botB.toggle(v));
   ["bg-dark", "navbar-dark"].map((v) => botB.toggle(v));
 
-}
-
-function isdarkMode() {
-  if (localStorage.dark) {
-    var element = document.body;
-    var c = document.getElementsByClassName("card");
-
-    for (let index = 0; index < c.length; index++) {
-      ["bg-dark", "text-white", "border-light", "border-dark"].map((v) =>
-        c[index].classList.toggle(v)
-      );
-    }
-
-    var nav = document.getElementById("nav").classList;
-    //   element.classList.toggle("bg-dark text-white");
-    ["bg-dark", "text-white"].map((v) => element.classList.toggle(v));
-    ["bg-light", "navbar-light"].map((v) => nav.toggle(v));
-    ["bg-dark", "navbar-dark"].map((v) => nav.toggle(v));
+  var search = document.getElementsByClassName("form-control");
+  for (let index = 0; index < search.length; index++) {
+    ["bg-dark", "text-white"].map((v) =>
+      search[index].classList.toggle(v))
     
-    var i = document.getElementById("i").classList;
-    i.toggle("i-black");
-
-    var botB = document.getElementById("botB").classList;
-    ["bg-light", "navbar-light"].map((v) => botB.toggle(v));
-    ["bg-dark", "navbar-dark"].map((v) => botB.toggle(v));
-
   }
 }
